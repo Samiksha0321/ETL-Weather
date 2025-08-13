b2fa09d90937735fa2f466299974cd2f603dd602
Samiksha0321 <samiksha0321@gmail.com>
2025-08-13 15:52:17 +0530

    extract_weather_data

--- dags/etlweather.py
+++ dags/etlweather.py
@@ -16,3 +16,28 @@ default_args = {
 }
 
 ## DAG
+with DAG(dag_id='weather_etl_pipeline',
+         default_args=default_args,
+         schedule_inteval='@daily',
+         catchup=False) as dag:
+    
+    @task()
+    def extract_weather_data():
+        """Extract weather data from Open-Meteo API using Airflow Connection."""
+        
+        # Use HTTP Hook to get the weather data
+        
+        http_hook=HttpHook(http_conn_id=API_CONN_ID, method='GET')
+        
+        
+        ## Build the API endpoint
+        endpoint= f'https://api.open-meteo.com/v1/forecast?latitude={LATITUDE}&longitude={LONGITUDE}&current_weather=TRUE'
+        
+        ## Make the request via the HTTP hook
+        response=http_hook.run(endpoint)
+        
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to fetch weather data: {response.status_code}")
+    
